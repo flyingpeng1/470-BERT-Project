17cef838ba52b7e17f430ad549bc3b6d73b57858
Jack Wilcom <jtwilcom@wilcomweb.net>
2021-12-11 01:35:58 -0500

    Update ProjectServer.py

--- src/qanta/ProjectServer.py
+++ src/qanta/ProjectServer.py
@@ -402,7 +402,7 @@ def buzztrain(vocab_file, buzzer_file, data_file, data_limit, num_epochs, link_f
     print("Training model", flush=True)
     model = LogRegModel(len(data[0][0]))
     agent = LogRegAgent(model, vocab, link_file)
-    agent.load_data(open(data_file))
+    agent.load_data(open(data_file), batch=100)
     agent.train(num_epochs, model, buzzer_file)
 
 
